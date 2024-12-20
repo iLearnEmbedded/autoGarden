fcdf60117d5690fd7c11e4238a2b749b8d668893
iLearnEmbedded <pawanembedded@gmail.com>
2024-12-20 07:01:19 +0530

    GPIO23 is configured as WiFi Reset pin

--- autoGarden_v2/autoGarden_v2.ino
+++ autoGarden_v2/autoGarden_v2.ino
@@ -1,15 +1,16 @@
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
-
+int WIFI_RST_SWITCH = 23;
 void setup() 
 {
-  Serial.begin(115200);    
-  setup_wifi();
-
+  WiFi.mode(WIFI_STA);
+  pinMode (WIFI_RST_SWITCH, INPUT_PULLUP);
+  Serial.begin(115200); 
 }
 
 void loop() 
 {
+    wifi_setup_task();  
     Serial.println("Hi");
-    delay(1000);
+    delay(1000);    
 }
--- autoGarden_v2/wifi_tasks.ino
+++ autoGarden_v2/wifi_tasks.ino
@@ -1,20 +1,22 @@
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
 
-void setup_wifi(void) 
+void wifi_setup_task(void) 
 {    
-    WiFiManager wm;    
-    wm.resetSettings();
-
-    bool res = wm.autoConnect("Garden_Bot"); // password protected ap
-
-    if(!res) {
-        Serial.println("Failed to connect");
-        // ESP.restart();
-    } 
-    else {
-        //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
+    if(digitalRead(WIFI_RST_SWITCH) == LOW)
+    {
+      WiFiManager wm;    
+      wm.resetSettings();
+  
+      bool res = wm.autoConnect("Garden_Bot"); // password protected ap
+  
+      if(!res) {
+          Serial.println("Failed to connect");
+          // ESP.restart();
+      } 
+      else {
+          //if you get here you have connected to the WiFi    
+          Serial.println("connected...yeey :)");
+      }  
     }
-
 }
