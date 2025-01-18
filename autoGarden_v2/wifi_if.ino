3630853deb1ba952b9f2a984fc628db0b57006ec
iLearnEmbedded <pawanembedded@gmail.com>
2025-01-18 10:57:30 +0530

    bug fix - wifi credentials not saved previously

--- autoGarden_v2/wifi_if.ino
+++ autoGarden_v2/wifi_if.ino
@@ -1,9 +1,11 @@
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
 int WIFI_RST_SWITCH = 23;
+WiFiManager wm;
 void wifi_setup(void)
 {
   Serial.begin(115200); 
+  wm.autoConnect();
   WiFi.mode(WIFI_STA);
   pinMode (WIFI_RST_SWITCH, INPUT_PULLUP);
 }
@@ -11,9 +13,8 @@ void wifi_task(void)
 {    
     if(digitalRead(WIFI_RST_SWITCH) == LOW)
     {
-      WiFiManager wm;    
+          
       wm.resetSettings();
-  
       bool res = wm.autoConnect("Garden_Bot"); // password protected ap
   
       if(!res) {
